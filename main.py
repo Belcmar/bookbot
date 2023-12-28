ff2b6bf5fab1e2b578146617c4e1d8e7fe708cb7
Belcmar <mark.belcourt@hotmail.com>
2023-12-28 09:37:35 -0600

    count characters

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ with open("book.txt",'r') as file:
 
     words = var.split()
     print("word count: ", len(var))
+    
+    char_dict = {}
+    for char in var:
+        if char not in char_dict:
+            char_dict[char] = 0
+
+        char_dict[char] += 1
+
+    print(char_dict)
